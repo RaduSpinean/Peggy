502aacea96d0961b12d58a49be6ca28a04dd1bcb
RaduSpinean <radu.spinean@yahoo.ro>
2015-04-06 18:58:10 +0300

    model

---
+++ application/models/Peggy_model.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Peggy_model extends CI_Model {
+	public function main_image(){
+		$query = $this->db->get('main_image');
+		return $query->result();
+	}
+	
+	public function get_round_image(){
+		$query = $this->db->get('round_pics');
+		return $query->result();
+	}
+	
+	public function get_content_home(){
+		$query = $this->db->get('content_home');
+		return $query->result();
+	}
+	
+	public function get_aside(){
+		$query = $this->db->get('aside');
+		return $query->result();
+	}
+	
+}
