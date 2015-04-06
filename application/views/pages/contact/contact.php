37d4bf1ad1b7ca8af99e0a7aa4c97c7c4d798dec
pelivansky <cpelivan30@yahoo.com>
2015-04-06 18:45:38 +0200

    contact

---
+++ application/views/pages/contact/contact.php
@@ -0,0 +1,22 @@
+	
+		<div class="section2">
+			<div class="section2_container1">
+				<div class="section2_main">
+					<div class="harta">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d40166.34955124137!2d-1.472427!3d51.008817!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sro!2sus!4v1425647168565"  ></iframe>
+					</div>
+					<div class="container_elemente">
+						<div class="div_formular">
+							<h1 class="titlu_about">contact me</h1>
+							<p>
+									This is my contact page, including a google map (if you want it) and a contact form, powered by Contact Form 7 (if you want it).
+							</p>
+							<form action="#" method="post" class="formular">
+								<input type="text" value="Name*" class="input_text" />
+								<input type="text" value="E-mail*" class="input_text" />
+								<textarea class="textarea">Your Message</textarea>
+								<input type="submit" value="send" class="submit" />
+							</form>
+						</div>
+					</div>
+				</div>
