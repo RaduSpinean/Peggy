a8181a4c5702237b595cb20cfa964bce359f4cbe
RaduSpinean <radu.spinean@yahoo.ro>
2015-04-07 12:08:01 +0300

    about view

---
+++ application/views/pages/about/about_content.php
@@ -0,0 +1,25 @@
+		<div class="section2"><!-- a doua sectiune -->
+			<div class="section2_container1"><!-- container mare sectiunea 2 -->
+				<div class="section2_main"><!-- container stanga sectiunea 2 -->
+					<div class="container_elemente"><!-- prima sectiune partea stanga -->
+						<div class="section2_about">
+							<h1 class="titlu_about">about me</h1>
+							<img src="../images/about-me.jpg" class="img_about" alt="about me"/><!-- prima sectiune partea stanga imagine -->
+							<!-- text prima sectiune partea stanga -->
+							<p>Hi, my name is Clare Smith, but my friends call me Peggy (don’t ask!) and I’m a serial blogger. I love life and more than anything in the whole wide world, I love taking photographs.
+							</p>
+							<p>Oh and did I mention, I quite like blogging?</p>
+							<p>
+							I’m an aspring photographer and this is my little corner of the world, so take a look around and let me know what you think… I’m on these little social networks too…</p><!-- end text prima sectiune partea stanga -->
+							<!-- links social prima sectiune partea stanga -->
+							<p><a href="http://twitter.com" class="socials_twitter">Twitter</a>
+								<a href="http://facebook.com" class="socials_facebook">Facebook</a>
+								
+								<a href="http://instagram.com" class="socials_instagram">Instagram</a>
+							<a href="http://flickr.com" class="socials_flickr">Flickr</a></p>
+							<!-- end links social prima sectiune partea stanga -->
+						</div>
+					</div>
+				</div>
+				
+					
