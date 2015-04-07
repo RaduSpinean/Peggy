264ff81d3fc85d38f4dc782b7b60fd0a55d3c2b6
RaduSpinean <radu.spinean@yahoo.ro>
2015-04-07 17:05:32 +0300

    modificari fisiere

--- application/views/pages/home/round_pics.php
+++ application/views/pages/home/round_pics.php
@@ -2,10 +2,47 @@
 			<h1 class="titlu1">My fave moments</h1>
 			<div class="section1_div1"><!-- container interior prima sectiune -->
 				<div class="content_div1"> <!-- container poze rotunde prima sectiune -->
-					<?php 			
-						if(is_array($results) and !empty($results))					
-							foreach($results as $row){?>
-								<div class="round_div">
+					<?php 		
+						$i = 0;
+						if(is_array($results) and !empty($results))			
+							
+							
+							
+						
+							foreach($results as $row){
+							
+							$i++;	
+							
+							switch($i)
+							{
+							case 2: 
+								$class = "pic2";
+							break;
+							
+							case 3: 
+							$class = "pic3";
+							break;
+							
+							case 4: 
+							$class = "pic4";
+							break;
+							
+							case 5: 
+							$class = "pic5";
+							break;
+							
+							case 6: 
+							$class = "pic6";
+							break;
+							
+							default:
+							$class = "";
+							
+							}
+							
+							
+							?>
+								<div class="round_div <?php echo $class;?>">
 									<a href="#"><img src="<?php echo base_url(); ?>assets/images/<?php echo $row->img_src; ?>" class="round_img" alt="<?php echo $row->img_alt; ?>"></a><!--  poza rotunda prima sectiune -->
 									<div class="section1_links"><!--  links sub poza rotunda-->
 									<a href="#" class="cute"><?php echo $row->categ; ?></a>
--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -4,12 +4,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url(); ?>assets/css/responsive.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url(); ?>assets/css/font-awesome.css" rel="stylesheet" type="text/css" />
 	<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" />
 	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css'>	
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>	
 	<title><?php echo $title; ?></title>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+
