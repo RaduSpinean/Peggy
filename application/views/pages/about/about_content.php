ac9e07cce349732f7048f90bcf328af375962ec3
pelivansky <cpelivan30@yahoo.com>
2015-04-07 11:38:53 +0200

    about file

--- application/views/pages/about/about_content.php
+++ application/views/pages/about/about_content.php
@@ -1,23 +1,21 @@
-		<div class="section2"><!-- a doua sectiune -->
-			<div class="section2_container1"><!-- container mare sectiunea 2 -->
-				<div class="section2_main"><!-- container stanga sectiunea 2 -->
-					<div class="container_elemente"><!-- prima sectiune partea stanga -->
+		<div class="section2">
+			<div class="section2_container1">
+				<div class="section2_main">
+					<div class="container_elemente">
 						<div class="section2_about">
-							<h1 class="titlu_about">about me</h1>
-							<img src="../images/about-me.jpg" class="img_about" alt="about me"/><!-- prima sectiune partea stanga imagine -->
-							<!-- text prima sectiune partea stanga -->
-							<p>Hi, my name is Clare Smith, but my friends call me Peggy (don’t ask!) and I’m a serial blogger. I love life and more than anything in the whole wide world, I love taking photographs.
-							</p>
-							<p>Oh and did I mention, I quite like blogging?</p>
-							<p>
-							I’m an aspring photographer and this is my little corner of the world, so take a look around and let me know what you think… I’m on these little social networks too…</p><!-- end text prima sectiune partea stanga -->
-							<!-- links social prima sectiune partea stanga -->
+							<?php 
+							if(isset($results) and !empty($results)){ ?>
+							<h1 class="titlu_about"><?php echo $results[0]->title; ?></h1>
+							<img src="<?php echo base_url(); ?>assets/images/<?php echo $results[0]->img_src; ?>" class="img_about" alt="<?php echo $results[0]->img_alt; ?>"/>
+							<p><?php echo $results[0]->par1; ?></p>
+							<p><?php echo $results[0]->par2; ?></p>
+							<p><?php echo $results[0]->par3; ?></p>
 							<p><a href="http://twitter.com" class="socials_twitter">Twitter</a>
 								<a href="http://facebook.com" class="socials_facebook">Facebook</a>
-								
 								<a href="http://instagram.com" class="socials_instagram">Instagram</a>
-							<a href="http://flickr.com" class="socials_flickr">Flickr</a></p>
-							<!-- end links social prima sectiune partea stanga -->
+								<a href="http://flickr.com" class="socials_flickr">Flickr</a>
+							</p>
+							<?php } ?>
 						</div>
 					</div>
 				</div>
